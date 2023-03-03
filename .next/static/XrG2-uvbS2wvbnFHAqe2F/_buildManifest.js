dd4d062df86fcd3130fd49b9f59e4b1badb9adbd
naufalhalim <naufalhalim46@gmail.com>
2023-03-04 00:18:05 +0700

    lisensi

---
+++ .next/static/XrG2-uvbS2wvbnFHAqe2F/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/89-496d83d439357f3d.js","static/chunks/pages/index-58f387e44cde7f46.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
